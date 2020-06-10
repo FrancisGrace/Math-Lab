312216deacd80d7c70ca87ea747a7ae53ba7bbd4
Francis Grace <59952277+FrancisGrace@users.noreply.github.com>
2020-06-10 09:19:31 +0800

    Update ex3.m

--- ex3.m
+++ ex3.m
@@ -1,10 +1,5 @@
-%任务1
-A=10+10*rand(10,10);%生成矩阵
-
-%任务2
-B=ceil(A);%取整
-C=B*diag(1:10);%将每列分别乘以标号
-
-%任务3
-[row,col]=find(C>=30 & C<=70);%寻找位于[30,70]之间的元素
-result=[row,col];%合并
+A=10+10*rand(10,10);
+B=ceil(A);
+C=B*diag(1:10);
+[row,col]=find(C>=30 & C<=70);
+res=[row,col];
