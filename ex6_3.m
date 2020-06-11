352a84a2c53046325c03f6d380900bed74f3ff00
Francis Grace <59952277+FrancisGrace@users.noreply.github.com>
2020-06-11 10:21:20 +0800

    Update ex6_3.m

--- ex6_3.m
+++ ex6_3.m
@@ -1,14 +1,12 @@
-%泰勒展开
 syms x;
 f=taylor(sin(x),'Order',3);
 x=3*pi/180;
-fprintf("(1)%s\n",eval(f));
+s1=eval(f);
 syms x;
 f=taylor(x^(1/3),x,27,'Order',3);
 x=30;
-fprintf("(2)%s\n",eval(f));
+s2=eval(f);
 syms x;
 f=taylor(sqrt(x),x,4,'Order',2);
-fprintf("(3)%s, ",f);
 x=4.4;
-fprintf("%f\n",eval(f));
+s3=eval(f);
