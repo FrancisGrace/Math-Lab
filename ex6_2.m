3e773e567c5e309b9398c5bd57d707275d721da3
Francis Grace <59952277+FrancisGrace@users.noreply.github.com>
2020-06-11 10:06:57 +0800

    Update ex6_2.m

--- ex6_2.m
+++ ex6_2.m
@@ -1,8 +1,4 @@
-%级数求和
 syms n;
-s=1/(n^2);
-fprintf("(1)%s\n",symsum(s,n,1,inf));
-s=((-1)^(n+1))/(2^n);
-fprintf("(2)%s\n",symsum(s,n,0,inf));
-s=((-1)^(n+1))*(x^(n+1))/(n*(n+1));
-fprintf("(3)%s\n",symsum(s,n,1,inf));
+s1=symsum(1/(n^2),n,1,inf);
+s2=symsum(((-1)^(n+1))/(2^n),n,0,inf);
+s3=symsum(((-1)^(n+1))*(x^(n+1))/(n*(n+1)),n,1,inf);
