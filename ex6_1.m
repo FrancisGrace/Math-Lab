afeffe5f84b291048cc05dbf39fa001491a8c40c
Francis Grace <59952277+FrancisGrace@users.noreply.github.com>
2020-06-11 10:05:26 +0800

    Update ex6_1.m

--- ex6_1.m
+++ ex6_1.m
@@ -1,10 +1,6 @@
-%求极限
-syms x n;
-f=sin(sin(x))/x-1;
-fprintf("(1)%s\n",limit(f,x,0));
-f=power(tan(pi/4+1/n),n);
-fprintf("(2)%s\n",limit(f,n,inf));
-f=x*(pi/2-asin(x/sqrt(x^2+1)));
-fprintf("(3)%s\n",limit(f,x,inf));
-f=1/(1+power(exp(1),1/x-1));
-fprintf("(4)%s,%s\n",limit(f,x,1,'left'),limit(f,x,1,'right'));
+syms x;
+s1=limit(sin(sin(x))/x-1,x,0);
+s2=limit(power(tan(pi/4+1/n),n),n,inf);
+s3=limit(x*(pi/2-asin(x/sqrt(x^2+1))),x,inf);
+s4_1=limit(1/(1+power(exp(1),1/x-1)),x,1,'left');
+s4_2=limit(1/(1+power(exp(1),1/x-1)),x,1,'right');
